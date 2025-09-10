bc82f8964d743fdd4c7d9a038a713c053df58967
patriotnewsactivism <patriotnewsactivism@gmail.com>
2025-09-10 18:17:59 -0500

    Resolve merge: keep local JS app for conflicting src/* files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './App.css';
+import './index.css';
 
-// Create the React root and render the App component.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
